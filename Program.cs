0f5e9d159d59a01d14ec7e59cadc8f97fcc39276
Jose Carlos Leon Tito <jleon@fips.local>
2024-07-23 22:19:40 -0500

    configuracion adicional

--- Program.cs
+++ Program.cs
@@ -6,8 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<SecurityContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("Security")));
+
+builder.Services.AddCors(options =>  {
+    options.AddPolicy("NuevaPolitica", app => {
+        app.AllowAnyOrigin()
+           .AllowAnyHeader()
+           .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddDbContext<SecurityContext>(
+    opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("Security")));
 
 // Register IUserService with its implementation UserService
 builder.Services.AddScoped<IUserService, UserService>();
@@ -16,17 +27,14 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-if (app.Environment.IsProduction())
+if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("NuevaPolitica");
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
--- obj/Debug/net8.0/SecurityApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/SecurityApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecurityApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9af739125d788139846acb3fbccc1e3a09c77c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e97952e03e592267f52a7f598d8518630b485406")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecurityApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecurityApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
